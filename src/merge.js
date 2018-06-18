c3e5f487a907b8532b4eeb9db456fe7c32ca6fc4
jmart <mail@jmartin.tf>
2018-06-18 10:30:07 +0200

    Add iso3 extents with country names

--- src/merge.js
+++ src/merge.js
@@ -13,10 +13,7 @@ function reduceUnits(acc, curr) {
 }
 
 function main() {
-  var iso3Extents = {};
-  var iso2Extents = {};
-  var countryNames = {};
-  var iso3ExtentsWithNames = {};
+  var iso3Extents = {}, iso2Extents = {};
   var unitsGeojson = JSON.parse(fs.readFileSync('./units.geojson'));  
   var iso3Iso2Json = JSON.parse(fs.readFileSync('./iso3-iso2.json'));
 
@@ -31,14 +28,10 @@ function main() {
     var geojson = ISO3s[ISO3];
     iso3Extents[ISO3] = bbox(geojson);
     iso2Extents[iso3ToIso2[ISO3]] = iso3Extents[ISO3];
-    countryNames[iso3ToIso2[ISO3]] = ISO3s[ISO3].features[0].properties.ADMIN;
-    iso3ExtentsWithNames[ISO3] = { e: iso3Extents[ISO3], n: ISO3s[ISO3].features[0].properties.ADMIN };
   });
 
   fs.writeFileSync('iso3-extents.json', JSON.stringify(iso3Extents));
   fs.writeFileSync('iso2-extents.json', JSON.stringify(iso2Extents));
-  fs.writeFileSync('country-names.json', JSON.stringify(countryNames));
-  fs.writeFileSync('iso3-extents-with-names.json', JSON.stringify(iso3ExtentsWithNames));
 }
 
 main();
